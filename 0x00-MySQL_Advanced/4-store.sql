6e8fbca381b139c950149fe7996b8653f1b1be40
hany0ageeb <hany0ageeb@gmail.com>
2024-10-16 09:57:08 +0000

    Task 4. Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,8 @@
 -- SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
 -- Quantity in the table items can be negative.
+-- Drop trigger if exists
+DROP TRIGGER IF EXISTS `ins_order`;
+-- create trigger per specifications
 CREATE TRIGGER `ins_order` AFTER INSERT ON `orders`
 	FOR EACH ROW
 		UPDATE `items`
