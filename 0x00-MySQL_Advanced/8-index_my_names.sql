d774347207182555b5659fabf27ca06d7817994f
hany0ageeb <hany0ageeb@gmail.com>
2024-10-17 10:41:40 +0000

    Task 8. Optimize simple search

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,6 @@
+-- SQL script that creates an index idx_name_first on the table names and the first letter of name.
+-- Requirements:
+-- 	1. Import this table dump: names.sql.zip
+--	2. Only the first letter of name must be indexed
+DROP INDEX IF EXISTS `idx_name_first`;
+CREATE INDEX `idx_name_first` ON `names`(`name`(1));
