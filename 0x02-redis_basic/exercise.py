bf79662e0cf8ab6ad47edfdc8bfed7706e8ddee3
hany0ageeb <hany0ageeb@gmail.com>
2024-10-23 09:06:05 +0000

    Task 3. Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -34,6 +34,21 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    call_history
+    """
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        inputs = [str(arg) for arg in args[1:]] + \
+            [v for k, v in kwargs.items() if k != 'self']
+        args[0].rpush("{}:inputs".format(method.__qualname__), *inputs)
+        output = [method(*args, **kwargs)]
+        args[0].rpush("{}:outputs".format(method.__qualname__), *output)
+        return output
+    return wrapper
+
+
 class Cache:
     """
     Cache class.
@@ -44,6 +59,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         takes a data argument and returns a string.
@@ -85,3 +101,7 @@ class Cache:
         wrapper around Redis.incr method
         """
         self._redis.incr(key)
+
+    def rpush(self, name: str, *args) -> None:
+        """Wrapper around Redis.rpush"""
+        self._redis.rpush(name, *args)
